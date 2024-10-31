df0e25fa077fe96b956309530bc80761889f066f
Richie Permana <richie.permana@gmail.com>
2024-11-01 04:43:58 +0800

    feat: prevent users doing multiple login when already logged

--- src/app/api/v1/auth/login/route.ts
+++ src/app/api/v1/auth/login/route.ts
@@ -37,7 +37,26 @@ export const POST = async (req: NextRequest) => {
     const valid = await bcrypt.compare(password, user.password);
 
     if (!valid) {
-      return NextResponse.json({ error: "Invalid Credentials" }, { status: 401 });
+      return NextResponse.json({ error: "Invalid Credentials." }, { status: 401 });
+    }
+
+    // if user still has an active session, throw an error and tell them they are already logged in
+    try {
+      const session = req.cookies.get("session");
+
+      if (session) {
+        jwt.verify(session.value, process.env.AUTH_SECRET);
+
+        return NextResponse.json(
+          {
+            message: "You are already logged in.",
+          },
+          { status: 400 }
+        );
+      }
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    } catch (error: unknown) {
+      // do nothing
     }
 
     const expiresIn = Number(process.env.AUTH_MAX_AGE) || 1 * 60 * 60 * 1000; // or 1 hour
