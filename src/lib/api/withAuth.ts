0245b06eadbea51cf96a694352d221b3634a4b72
Richie Permana <richie.permana@gmail.com>
2024-11-01 04:44:56 +0800

    feat: added additional jwt token check for logged users

--- src/lib/api/withAuth.ts
+++ src/lib/api/withAuth.ts
@@ -1,8 +1,9 @@
+import jwt from "jsonwebtoken";
 import { RequestCookie } from "next/dist/compiled/@edge-runtime/cookies";
 import { NextRequest, NextResponse } from "next/server";
 
 export const withAuth = (handler: (req: NextRequest, session: RequestCookie) => Promise<NextResponse>) => {
-  return async (req: NextRequest) => {
+  return async (req: NextRequest, { params }: { params: Promise<{ slug: string }> }) => {
     const session = req.cookies.get("session");
 
     if (!session?.value) {
@@ -10,6 +11,25 @@ export const withAuth = (handler: (req: NextRequest, session: RequestCookie) => 
         message: "User is not authenticated",
         success: false,
       });
+    }
+
+    try {
+      jwt.verify(session.value, process.env.AUTH_SECRET);
+    } catch (err) {
+      const error = err as Error;
+
+      const response = NextResponse.json({
+        message: "User session is invalid",
+        reason: error.message,
+        success: false,
+      });
+
+      response.cookies.set("session", "", {
+        httpOnly: true,
+        maxAge: 0,
+      });
+
+      return response;
     }
 
     return handler(req, session);
