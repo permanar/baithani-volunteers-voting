5257b8ee076c661d5d095f5fd11a831524eeb134
Richie Permana <richie.permana@gmail.com>
2024-11-01 04:34:01 +0800

    feat: improve seeder and added for roles

--- src/db/mysql2/seeder.ts
+++ src/db/mysql2/seeder.ts
@@ -1,41 +1,9 @@
-import Papa from "papaparse";
-import { promises as fs } from "fs";
-import bcrypt from "bcryptjs";
-
-import { db } from "./mysql";
-import { users } from "./schema";
-
-type UserCsvType = {
-  name: string;
-  username: string;
-  password: string;
-};
+import { seedUsers, seedRoles, seedUserRoles } from "./seeders";
 
 async function main() {
-  console.log(`🌱 Seeding: users...`);
-  console.time("Seeding time");
-
-  const data = await fs.readFile("./csv/pelayan-no-duplicate.csv", "utf-8");
-  const usersCsv = Papa.parse<UserCsvType>(data, {
-    header: true,
-  });
-
-  const allUsers = usersCsv.data.map((user) => {
-    const full_name = user.name;
-    const username = user.username;
-    const password = bcrypt.hashSync(user.password, 10);
-
-    return {
-      full_name,
-      username,
-      password,
-    };
-  });
-
-  await db.insert(users).values(allUsers).execute();
-
-  console.log(`✅ Seeding users done: ${allUsers.length} rows.`);
-  console.timeEnd("Seeding time");
+  await seedUsers();
+  await seedRoles();
+  await seedUserRoles();
 }
 
 main()
---
+++ src/db/mysql2/seeders/index.ts
@@ -0,0 +1,3 @@
+export * from "./role.seed";
+export * from "./user.seed";
+export * from "./userRole.seed";
