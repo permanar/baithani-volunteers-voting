fb0b1bc025aaaa0fa41a8f6968b78a20aac7ec29
Richie Permana <richie.permana@gmail.com>
2024-11-04 01:52:21 +0800

    feat: updated VoterSection

--- src/components/Pages/homepage/HomeVoterSection/HomeVoterSection.tsx
+++ src/components/Pages/homepage/HomeVoterSection/HomeVoterSection.tsx
@@ -7,9 +7,10 @@
 
 import React from "react";
 
-import { IconMenuHalfBurger, IconSearch } from "@/components/Partials/Icons";
+import { IconBgGradient, IconMenuHalfBurger, IconSearch } from "@/components/Partials/Icons";
 import { InputText } from "@/components/Partials/Input";
 import { VolunteerResponse } from "@/types";
+import { VoteCard } from "../HomeHeroSection/components";
 
 type Props = {
   data: VolunteerResponse;
@@ -22,6 +23,10 @@ export const HomeVoterSection = (props: Props) => {
 
   return (
     <div className="relative flex flex-col gap-5 mt-16 px-6">
+      <div className="absolute -top-96 left-0 -z-10">
+        <IconBgGradient />
+      </div>
+
       <div className="flex flex-col gap-2.5">
         <div className="flex items-center justify-between">
           <h2 className="text-xl font-semibold">Bidang Pelayanan</h2>
@@ -47,27 +52,7 @@ export const HomeVoterSection = (props: Props) => {
 
       <div className="flex flex-col gap-8 row-start-2 items-start">
         {data && data.length > 0
-          ? data.map((user) => (
-              <div key={user.id} className="flex gap-4 items-center">
-                <div className="w-10 h-10 bg-black/[.05] dark:bg-white/[.06] rounded-full flex items-center justify-center">
-                  {user.full_name[0]}
-                </div>
-                <div>
-                  <h3 className="text-lg font-semibold">{user.full_name}</h3>
-
-                  <div className="flex flex-wrap gap-2">
-                    {user.volunteer_categories.map((category, idx) => (
-                      <span
-                        key={`${category.name}-${idx}`}
-                        className="rounded-full bg-black/[.05] dark:bg-white/[.06] text-xs px-2 py-0.5"
-                      >
-                        {category.name}
-                      </span>
-                    ))}
-                  </div>
-                </div>
-              </div>
-            ))
+          ? data.map((user) => <VoteCard key={`vote-card-${user.id}-${user.username}`} data={user} />)
           : null}
       </div>
     </div>
