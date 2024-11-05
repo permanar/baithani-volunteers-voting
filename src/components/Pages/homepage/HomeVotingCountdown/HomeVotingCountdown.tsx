21705005968e5f066846399aa0294f60c8fbd28d
Richie Permana <richie.permana@gmail.com>
2024-11-06 01:43:48 +0800

    feat: implemented countdown vote result timer

--- src/components/Pages/homepage/HomeVotingCountdown/HomeVotingCountdown.tsx
+++ src/components/Pages/homepage/HomeVotingCountdown/HomeVotingCountdown.tsx
@@ -5,13 +5,51 @@
  * @author Richie Permana <richie.permana@gmail.com>
  */
 
+"use client";
+
+import React, { useEffect, useState } from "react";
+
+import { intervalToDuration } from "date-fns";
+
 import { MainButton } from "@/components/Partials/Button";
-import React from "react";
+import { VOTING } from "@/common/constants";
 
 type Props = {};
 
 export const HomeVotingCountdown = (props: Props) => {
   const {} = props;
+  const [timeLeft, setTimeLeft] = useState({
+    days: 0,
+    hours: 0,
+    minutes: 0,
+    seconds: 0,
+  });
+
+  useEffect(() => {
+    const getTimeLeft = (endDate: string) => {
+      const duration = intervalToDuration({
+        start: new Date(),
+        end: new Date(endDate),
+      });
+
+      return {
+        days: duration.days || 0,
+        hours: duration.hours || 0,
+        minutes: duration.minutes || 0,
+        seconds: duration.seconds || 0,
+      };
+    };
+
+    // if voting is still ongoing, update the time left every second
+    let interval: NodeJS.Timeout;
+    if (new Date(VOTING.END_DATE) > new Date()) {
+      interval = setInterval(() => {
+        setTimeLeft(getTimeLeft(VOTING.END_DATE));
+      }, 1000);
+    }
+
+    return () => clearInterval(interval);
+  }, []);
 
   return (
     <div className="relative flex flex-col gap-8 mt-16 px-6">
@@ -25,7 +63,7 @@ export const HomeVotingCountdown = (props: Props) => {
       <div className="flex items-center justify-center gap-4">
         <div className="flex flex-col gap-1">
           <div className="flex items-center justify-center w-11 h-11 rounded p-3 bg-purple/50">
-            <span className="text-center text-lg font-bold text-white-900">10</span>
+            <span className="text-center text-lg font-bold text-white-900"> {timeLeft.days} </span>
           </div>
           <span className="text-center text-xs text-black/65">hari</span>
         </div>
@@ -33,7 +71,7 @@ export const HomeVotingCountdown = (props: Props) => {
 
         <div className="flex flex-col gap-1">
           <div className="flex items-center justify-center w-11 h-11 rounded p-3 bg-purple/50">
-            <span className="text-center text-lg font-bold text-white-900">05</span>
+            <span className="text-center text-lg font-bold text-white-900"> {timeLeft.hours} </span>
           </div>
           <span className="text-center text-xs text-black/65">jam</span>
         </div>
@@ -41,7 +79,7 @@ export const HomeVotingCountdown = (props: Props) => {
 
         <div className="flex flex-col gap-1">
           <div className="flex items-center justify-center w-11 h-11 rounded p-3 bg-purple/50">
-            <span className="text-center text-lg font-bold text-white-900">22</span>
+            <span className="text-center text-lg font-bold text-white-900"> {timeLeft.minutes} </span>
           </div>
           <span className="text-center text-xs text-black/65">menit</span>
         </div>
@@ -49,7 +87,7 @@ export const HomeVotingCountdown = (props: Props) => {
 
         <div className="flex flex-col gap-1">
           <div className="flex items-center justify-center w-11 h-11 rounded p-3 bg-purple/50">
-            <span className="text-center text-lg font-bold text-white-900">18</span>
+            <span className="text-center text-lg font-bold text-white-900"> {timeLeft.seconds} </span>
           </div>
           <span className="text-center text-xs text-black/65">detik</span>
         </div>
